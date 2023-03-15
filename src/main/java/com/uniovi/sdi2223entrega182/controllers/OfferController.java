0882b767462e35e684741aae1e3b979ccfecc2d3
uo276723 <uo276723@uniovi.es>
2023-03-15 14:52:48 +0100

    Eliminar ofertas y validacion de datos en el servidor para crear oferta

--- src/main/java/com/uniovi/sdi2223entrega182/controllers/OfferController.java
+++ src/main/java/com/uniovi/sdi2223entrega182/controllers/OfferController.java
@@ -6,15 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import com.uniovi.sdi2223entrega182.validators.AddOfferValidator;
 
 @Controller
 public class OfferController {
 
     @Autowired
     private OffersService offersService;
+
+    @Autowired
+    private AddOfferValidator addOfferValidator;
 
     @RequestMapping("/offer/list")
     public String getList(Model model){
@@ -23,13 +30,24 @@ public class OfferController {
     }
 
     @RequestMapping(value = "/offer/add")
-    public String getOffer() {
+    public String getOffer(Model model) {
+        model.addAttribute("offer", new Offer());
         return "offer/add";
     }
 
     @RequestMapping(value = "/offer/add", method = RequestMethod.POST)
-    public String setOffer(@ModelAttribute Offer offer) {
+    public String setOffer(@Validated Offer offer, BindingResult result) {
+        addOfferValidator.validate(offer, result);
+        if (result.hasErrors()) {
+            return "offer/add";
+        }
         offersService.addOffer(offer);
+        return "redirect:/offer/list";
+    }
+
+    @RequestMapping("/offer/delete/{id}")
+    public String deleteOffer(@PathVariable Long id){
+        offersService.deleteOffer(id);
         return "redirect:/offer/list";
     }
 
