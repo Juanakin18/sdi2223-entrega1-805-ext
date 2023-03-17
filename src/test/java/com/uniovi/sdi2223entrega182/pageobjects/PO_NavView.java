3adf0737ac02ab71243cb4d7f638fa46b76e399d
Alonso Antuña Gómez <uo282834@uniovi.es>
2023-03-17 12:12:09 +0100

    Test

---
+++ src/test/java/com/uniovi/sdi2223entrega182/pageobjects/PO_NavView.java
@@ -0,0 +1,4 @@
+package com.uniovi.sdi2223entrega182.pageobjects;
+
+public class PO_NavView extends PO_View{
+}
---
+++ src/test/java/com/uniovi/sdi2223entrega182/pageobjects/PO_PrivateView.java
@@ -0,0 +1,4 @@
+package com.uniovi.sdi2223entrega182.pageobjects;
+
+public class PO_PrivateView extends PO_NavView{
+}
